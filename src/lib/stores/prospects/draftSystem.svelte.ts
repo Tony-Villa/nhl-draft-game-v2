2cd898c60e1c5a072e7eaadf04ea492c4dc37ed7
Tony-Villa <tonyvilla77@gmail.com>
2024-01-17 00:29:28 -0800

    feat: add and remove prospect methods (local only)

--- src/lib/stores/prospects/draftSystem.svelte.ts
+++ src/lib/stores/prospects/draftSystem.svelte.ts
@@ -1,24 +1,41 @@
 export function draftSystem(prospectList, draftboard) {
 	const topProspects = $state(prospectList);
 	const draftBoard = $state(draftboard);
+	const shownProspects = $state(prospectList);
 
 	function addProspectToBoard(prospect, position = 1) {
-		console.log(draftBoard);
-		console.log(prospect);
-
 		draftBoard[position - 1].prospect = prospect;
-		console.log(draftBoard);
+		shownProspects.forEach((p) => {
+			if (p.name === prospect.name) {
+				p.drafted = true;
+			}
+		});
+	}
+
+	function removeProspectFromBoard(prospect, position = 1) {
+		draftBoard[position - 1].prospect = null;
+		shownProspects.forEach((p) => {
+			if (p.name === prospect.name) {
+				p.drafted = false;
+			}
+		});
 	}
 
 	return {
 		get topProspects() {
 			return topProspects;
 		},
+		get shownProspects() {
+			return shownProspects;
+		},
 		get draftBoard() {
 			return draftBoard;
 		},
 		get addProspectToBoard() {
 			return addProspectToBoard;
+		},
+		get removeProspectFromBoard() {
+			return removeProspectFromBoard;
 		}
 	};
 }
