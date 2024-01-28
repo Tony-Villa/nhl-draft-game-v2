45fb9422adfd0462f0999305f0607564a3967136
Tony-Villa <tonyvilla77@gmail.com>
2024-01-28 00:31:42 -0800

    BREAKING CHANGE: remove supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,9 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		// interface Locals
+		// interface PageData
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
 		// interface Platform {}
 	}
 }
