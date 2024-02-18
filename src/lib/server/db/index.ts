08d8906fff0267665fb056912c337816dc81690e
Tony-Villa <tonyvilla77@gmail.com>
2024-02-18 12:57:05 -0800

    fix: use local db for dev environment

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,12 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import { VITE_TURSO_DB_URL, VITE_TURSO_DB_AUTH_TOKEN } from '$env/static/private';
+import { dev } from '$app/environment';
+
+const url = dev ? 'http://127.0.0.1:8080' : VITE_TURSO_DB_URL;
 
 const client = createClient({
-	url: VITE_TURSO_DB_URL,
+	url,
 	authToken: VITE_TURSO_DB_AUTH_TOKEN
-	// url: 'http://127.0.0.1:8080'
 });
 export const db = drizzle(client);
