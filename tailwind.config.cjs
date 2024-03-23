27bd19d7379e17e7f7db7ea9f8b3053fe226f5e2
Tony-Villa <tonyvilla77@gmail.com>
2024-03-23 00:32:47 -0700

    feat: add brutalist shadow to tw config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			boxShadow: {
+				'brut-shadow': '4px 4px 0px 0px #000000',
+				'brut-shadow-sm': '2px 2px 0px 0px #000000',
+			}
+		}
 	},
 
 	plugins: []
