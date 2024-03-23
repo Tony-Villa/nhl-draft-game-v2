549ead5facfb8801fbf88a151e0e171d9daa91b7
Tony-Villa <tonyvilla77@gmail.com>
2024-03-23 00:34:47 -0700

    fix: go back to not using brut shadow on card by default

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,13 +2,7 @@
   const { children, variant} : {children: any; variant?: string | null} = $props();
 </script>
 
-<div class={`border-2 border-black rounded-md shadow-brut-shadow ${variant === 'small' ? 'p-0' : 'p-3'}`}>
+<div class={`border-2 border-black rounded-md ${variant === 'small' ? 'p-0' : 'p-3'}`}>
   {@render children()}
 </div>
-
-<!-- <style lang='postcss'>
-  .brut-shadow {
-    box-shadow: 4px 4px 0px 0px #000000;
-  }
-</style> -->
 
