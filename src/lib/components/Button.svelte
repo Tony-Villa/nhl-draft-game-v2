72d2dc2b836adb826aeee9cae13eaa912a708190
Tony-Villa <tonyvilla77@gmail.com>
2024-03-23 00:33:04 -0700

    chore: switch to brutalist shadow instead of border

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -10,7 +10,7 @@
 	const { children, variant = 'primary' }: ButtonProps = $props();
 </script>
 
-<button on:click class={`border-2 border-b-4 border-r-4 rounded-md border-solid border-gray-900 px-3 py-1 relative  ${variant === 'primary' ? 'bg-yellow-400 font-semibold' : variant === 'secondary' ? 'bg-orange-100' : ''} `}>
+<button on:click class={`border-2 shadow-brut-shadow-sm rounded-md border-solid border-gray-900 px-3 py-1 relative  ${variant === 'primary' ? 'bg-yellow-400 font-semibold' : variant === 'secondary' ? 'bg-orange-100' : ''} `}>
   <!-- {#if variant === 'primary'}
   <div class="absolute h-full w-full bg-yellow-400 top-1 left-1  rounded z-0" />
   {/if} -->
