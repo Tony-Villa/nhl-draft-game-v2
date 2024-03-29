4e0fdd63ef3ea122f3f6263b844332c4069a2de3
Tony-Villa <tonyvilla77@gmail.com>
2024-03-28 18:07:54 -0700

    fix: add search icon

--- src/lib/components/Searchbar.svelte
+++ src/lib/components/Searchbar.svelte
@@ -8,4 +8,12 @@
 	} = $props();
 </script>
 
-<input class="text-black" type="text" bind:value placeholder={placeholder ? placeholder : ''} />
+
+
+<div class="flex text-black rounded-3xl bg-orange-200 p-2 px-3 gap-1 placeholder:text-slate-500">
+	<svg viewBox='0 0 24 24' width="24" height='24' fill='#3f3f46'>
+		<path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
+	</svg>
+
+	<input class="bg-transparent focus:outline-none" type="text" bind:value placeholder={placeholder ? placeholder : ''} />
+</div>
