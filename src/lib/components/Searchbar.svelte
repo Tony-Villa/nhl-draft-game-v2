2fcc3e5e3598f36cbfc3c7ea138d898f9cfcc7a9
Tony-Villa <tonyvilla77@gmail.com>
2024-04-04 16:59:18 -0700

    fix: serachbar styles

--- src/lib/components/Searchbar.svelte
+++ src/lib/components/Searchbar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Close from '$lib/icons/Close.svelte';
+
 	let {
 		value,
 		placeholder
@@ -8,21 +10,29 @@
 	} = $props();
 
 	let isFocused = $state(false);
-
 </script>
 
-
-
-<div class={`flex text-black rounded-xl shadow-brut-shadow p-2 px-3 gap-1 lg:w-auto xl:w-[400px] ${isFocused ? 'bg-cyan-300' : 'bg-cyan-200'}`}>
-	<svg viewBox='0 0 24 24' width="24" height='24' fill='#3f3f46'>
-		<path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
+<div
+	class={`flex gap-1 rounded-xl border-2 border-black p-2 px-3 text-black shadow-brut-shadow-sm lg:w-auto xl:w-[400px] ${isFocused ? 'bg-orange-100' : 'bg-orange-50'}`}
+>
+	<svg viewBox="0 0 24 24" width="24" height="24" fill="#3f3f46">
+		<path
+			d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+		></path>
 	</svg>
 
-	<input class=" flex-grow bg-transparent focus:outline-none placeholder-gray-950" type="text" bind:value on:focus={() => isFocused = true} on:blur={() => isFocused = false} placeholder={placeholder ? placeholder : ''} />
+	<input
+		class="flex-grow bg-transparent placeholder-gray-950 focus:outline-none"
+		type="text"
+		bind:value
+		on:focus={() => (isFocused = true)}
+		on:blur={() => (isFocused = false)}
+		placeholder={placeholder ? placeholder : ''}
+	/>
 
-	<div class="justify-self-end border-2 border-black rounded-full px-2">
-		<button on:click={() => value = ''}>
-			<p class='font-bold'>X</p>
+	{#if value.length}
+		<button on:click={() => (value = '')}>
+			<Close size={24} />
 		</button>
-	</div>
+	{/if}
 </div>
