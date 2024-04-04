5d033ba4c2f8ed48f366e5e0b4e6b1640ebefc02
Tony-Villa <tonyvilla77@gmail.com>
2024-04-03 21:10:22 -0700

    fix: pass class to card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  const { children, variant, class: classname} : {children: any; variant?: string | null; class?: string;} = $props();
+  const { children, variant, class: className} : {children: any; variant?: string | null; class?: string;} = $props();
 </script>
 
-<div class={`border-2 border-black rounded-md ${variant === 'small' ? 'p-0' : 'p-3'} ${classname}`}>
+<div class={`border-2 border-black rounded-md ${variant === 'small' ? 'p-0' : 'p-3'} ${className}`}>
   {@render children()}
 </div>
 
