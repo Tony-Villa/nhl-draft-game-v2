613d9e156b9bba5030145c45205a0b573fd25733
Tony-Villa <tonyvilla77@gmail.com>
2024-04-03 23:36:49 -0700

    fix: focus and blur styles

--- src/lib/components/Searchbar.svelte
+++ src/lib/components/Searchbar.svelte
@@ -6,20 +6,23 @@
 		value: string;
 		placeholder?: string;
 	} = $props();
+
+	let isFocused = $state(false);
+
 </script>
 
 
 
-<div class="flex text-black rounded-xl shadow-brut-shadow bg-cyan-400 p-2 px-3 gap-1 lg:w-auto xl:w-[400px]">
+<div class={`flex text-black rounded-xl shadow-brut-shadow p-2 px-3 gap-1 lg:w-auto xl:w-[400px] ${isFocused ? 'bg-cyan-300' : 'bg-cyan-200'}`}>
 	<svg viewBox='0 0 24 24' width="24" height='24' fill='#3f3f46'>
 		<path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
 	</svg>
 
-	<input class=" flex-grow bg-transparent focus:outline-none placeholder-gray-950" type="text" bind:value placeholder={placeholder ? placeholder : ''} />
+	<input class=" flex-grow bg-transparent focus:outline-none placeholder-gray-950" type="text" bind:value on:focus={() => isFocused = true} on:blur={() => isFocused = false} placeholder={placeholder ? placeholder : ''} />
 
 	<div class="justify-self-end border-2 border-black rounded-full px-2">
 		<button on:click={() => value = ''}>
-			<p>X</p>
+			<p class='font-bold'>X</p>
 		</button>
 	</div>
 </div>
