b3aea74c37c15ef2a9ac3380ae669aeafea217d6
Tony-Villa <tonyvilla77@gmail.com>
2024-04-12 22:29:02 -0700

    fix: add button hover color

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -12,7 +12,7 @@
 	const { children, disabled = false,  variant = 'primary', class: className }: ButtonProps = $props();
 </script>
 
-<button disabled={disabled} on:click class={`border-2 shadow-brut-shadow-sm rounded-md border-solid border-black px-3 py-1 relative  
+<button disabled={disabled} on:click class={`border-2 shadow-brut-shadow-sm rounded-md border-solid border-black px-3 py-1 relative hover:bg-yellow-500 
 ${disabled ? 'bg-neutral-400' : variant === 'primary' ? 'bg-yellow-400 font-semibold' : variant === 'secondary' ? 'bg-orange-100' : ''} ${className}`}>
     {@render children()}
 </button>
