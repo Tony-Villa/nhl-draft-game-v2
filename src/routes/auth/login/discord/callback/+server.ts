2430ec65e61330efd384aec80ea3626ab1088201
Tony-Villa <tonyvilla77@gmail.com>
2024-04-14 22:59:25 -0700

    chore: temp remove logs

--- src/routes/auth/login/discord/callback/+server.ts
+++ src/routes/auth/login/discord/callback/+server.ts
@@ -13,12 +13,6 @@ export async function GET(event: RequestEvent): Promise<Response> {
 	const state = event.url.searchParams.get('state');
 	const storedState = event.cookies.get('discord_oauth_state') ?? null;
 
-	console.log('Are we in the discord callback?', {
-		code,
-		state,
-		storedState
-	});
-
 	if (!code || !state || !storedState || state !== storedState) {
 		console.log('Invalid OAuth state or code verifier');
 		return new Response('Invalid OAuth state or code verifier', {
