b50dbfe4ee09b3ed69307d6c36fd83176b2eb1eb
Tony-Villa <tonyvilla77@gmail.com>
2024-04-15 20:41:34 -0700

    fix: remove hover state on mobile screens

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -13,7 +13,8 @@
 	const { children, disabled = false,  variant = 'primary', class: className, id }: ButtonProps = $props();
 </script>
 
-<button disabled={disabled} id={id} on:click class={`border-2 shadow-brut-shadow-sm rounded-md border-solid border-black px-3 py-1 relative ${!disabled && 'hover:bg-yellow-500'}
+<button disabled={disabled} id={id} on:click class={`border-2 shadow-brut-shadow-sm rounded-md border-solid border-black px-3 py-1 relative ${!disabled && 'sm:hover:bg-yellow-500'}
+}
 ${disabled ? 'bg-neutral-400' : variant === 'primary' ? 'bg-yellow-400 font-semibold' : variant === 'secondary' ? 'bg-orange-100' : ''} ${className}`}>
     {@render children()}
 </button>
