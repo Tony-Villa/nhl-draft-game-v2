ef6d207ec0ca26511dc7d23677e4062ec5300075
Tony-Villa <tonyvilla77@gmail.com>
2024-04-16 23:38:25 -0700

    fix: log error in correct utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,26 @@ import { twMerge } from "tailwind-merge";
 import { cubicOut } from "svelte/easing";
 import type { TransitionConfig } from "svelte/transition";
 
+export function logError(
+	error: unknown,
+	context = 'General context',
+	extraInfo: Record<string, unknown> = {}
+) {
+	const errorObject = error instanceof Error ? error : new Error(String(error));
+
+	console.error({
+		timestamp: new Date().toISOString(),
+		context,
+		error: {
+			name: errorObject.name ?? 'No name available',
+			message: errorObject.message ?? 'No message available',
+			stack: errorObject.stack ?? 'No stack trace available'
+		},
+		extraInfo
+	});
+}
+
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
