be1594bdde45a8d0eb3540371d8b05f81e2447b6
Tony-Villa <tonyvilla77@gmail.com>
2024-04-16 22:31:06 -0700

    feat: use drawer instead of modal for prospect draft

--- src/lib/components/ProspectCard.svelte
+++ src/lib/components/ProspectCard.svelte
@@ -8,6 +8,7 @@
 	import type { Prospect, DraftBoard } from '$lib/types';
 	import Button from './Button.svelte';
 	import Card from './Card.svelte';
+	import * as Drawer from "$lib/components/ui/drawer/index.js";
 
 	let showModal = $state(false);
 
@@ -31,58 +32,78 @@
 	}
 </script>
 
-<Card variant='small'>
-	<div class="prospect-card relative flex w-48 flex-col content-between gap-2 pb-4 px-4 min-h-[235px] h-full md:w-52 max-[430px]:w-44 max-[399px]:w-[165px]">
-		<!-- TODO: play with this idea (probably dumb af tho) -->
-		<img
-			src={prospect?.nation}
-			alt="nationality"
-			class="flag absolute left-0 top-0 w-10 object-cover opacity-40"
-		/>
-		<Modal bind:showModal on:close={() => showModal = false}>
-			<div class="flex flex-wrap justify-center gap-2">
-				{#each draftSystem.draftBoard as cell}
-					{@render teamPicker(cell)}
-				{/each}
-			</div>
-		</Modal>
 
-		<div class="flex justify-between">
-			<p class="font-semibold">{prospect?.rank !== '-' ? 'Rank: ' + prospect?.rank : 'NR'}</p>
-			<div class="flex content-end justify-end gap-2 text-xs opacity-55">
-				<p>{prospect?.position}</p>
-				<p>|</p>
-				<p>{prospect?.shoots}</p>
+<Drawer.Root>
+	<Card variant='small'>
+		<div class="prospect-card relative flex w-48 flex-col content-between gap-2 pb-4 px-4 min-h-[235px] h-full md:w-52 max-[430px]:w-44 max-[399px]:w-[165px]">
+			<img
+				src={prospect?.nation}
+				alt="nationality"
+				class="flag absolute left-0 top-0 w-10 object-cover opacity-40"
+			/>
+			<Modal bind:showModal on:close={() => showModal = false}>
+				<div class="flex flex-wrap justify-center gap-2">
+					{#each draftSystem.draftBoard as cell}
+						{@render teamPicker(cell)}
+					{/each}
+				</div>
+			</Modal>
+	
+			<div class="flex justify-between">
+				<p class="font-semibold">{prospect?.rank !== '-' ? 'Rank: ' + prospect?.rank : 'NR'}</p>
+				<div class="flex content-end justify-end gap-2 text-xs opacity-55">
+					<p>{prospect?.position}</p>
+					<p>|</p>
+					<p>{prospect?.shoots}</p>
+				</div>
+			</div>
+	
+			<div class="flex  flex-1 flex-col justify-between">
+					<h3 class="text-center font-bold text-lg mt-3 whitespace-break-spaces leading-6 max-w-[14ch]">{prospect?.name}</h3>
+		
+					<div class="flex flex-col text-center mt-2 content-center justify-center gap-[0]">
+						<p class="text-sm font-semibold">{prospect?.league}</p>
+						<p class="text-xs opacity-55">{prospect?.team}</p>
+					</div>
+			
+					<div class="flex flex-col gap-3 justify-end">
+		
+					<div class="flex content-end justify-between gap-2 text-xs opacity-55">
+						<p>{prospect?.birthDay}</p>
+						<p>{prospect?.height}</p>
+						<p>{prospect?.weight} lbs</p>
+					</div>
+			
+					<div class="flex items-end justify-center gap-2">
+						<Drawer.Trigger>
+							<div class={`border-2 shadow-brut-shadow-sm rounded-md border-solid border-black px-3 py-1 relative 
+						bg-yellow-400 font-semibold`}>Draft</div>
+						</Drawer.Trigger>
+					</div>
+				</div>
 			</div>
 		</div>
+	</Card>
+	<Drawer.Content class='bg-[#FFF4E8]'>
+    <Drawer.Header class='md:mx-auto'>
+      <Drawer.Title>Who will be drafting {prospect.name}?</Drawer.Title>
+    </Drawer.Header>
 
-		<div class="flex  flex-1 flex-col justify-between">
-				<h3 class="text-center font-bold text-lg mt-3 whitespace-break-spaces leading-6 max-w-[14ch]">{prospect?.name}</h3>
-	
-				<div class="flex flex-col text-center mt-2 content-center justify-center gap-[0]">
-					<p class="text-sm font-semibold">{prospect?.league}</p>
-					<p class="text-xs opacity-55">{prospect?.team}</p>
-				</div>
-		
-				<div class="flex flex-col gap-3 justify-end">
-	
-				<div class="flex content-end justify-between gap-2 text-xs opacity-55">
-					<p>{prospect?.birthDay}</p>
-					<p>{prospect?.height}</p>
-					<p>{prospect?.weight} lbs</p>
-				</div>
-		
-				<div class="flex items-end justify-center gap-2">
-					<Button on:click={() => (showModal = !showModal)}>Draft</Button>
-				</div>
-			</div>
+		<div class="flex flex-wrap justify-center gap-2 pb-10 md:max-w-[600px] md:mx-auto">
+			{#each draftSystem.draftBoard as cell}
+				{@render teamPicker(cell)}
+			{/each}
 		</div>
-	</div>
-</Card>
+
+  </Drawer.Content>
+</Drawer.Root>
+
+
 
 {#snippet teamPicker(cell)}
 	<button
-		class={`flex flex-col content-center justify-center gap-2 border border-black rounded-md p-2 w-20 h-20 md:w-28 md:h-28 ${!!cell?.prospect ? 'bg-neutral-400' : 'bg-orange-100'}`}
+		class={`flex flex-col content-center justify-center gap-2 border border-black rounded-md p-2 w-20 h-20 md:w-28 md:h-28
+		 ${!!cell?.prospect ? '' : 'hover:bg-[#f2dabd]'} ${!!cell?.prospect ? 'bg-neutral-400' : 'bg-orange-100'}`}
 		on:click={() => draft(prospect as Prospect, cell?.draftPosition)}
 		disabled={!!cell?.prospect}
 	>
