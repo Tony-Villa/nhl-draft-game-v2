7d72bf6dcd7b158614a38136c4be42aca1801283
Tony-Villa <tonyvilla77@gmail.com>
2024-04-25 16:45:41 -0700

    chore: svelte 5 drawer cleanup

--- src/lib/components/ui/drawer/drawer.svelte
+++ src/lib/components/ui/drawer/drawer.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
 	import { Drawer as DrawerPrimitive } from "vaul-svelte";
+	import type { Snippet } from 'svelte'
 
 	type $$Props = DrawerPrimitive.Props;
 
 	let {children, shouldScaleBackground, open = false, activeSnapPoint, ...rest}: 
 	{
-		children: any;
-		shouldScaleBackground: $$Props["shouldScaleBackground"];
-		open:  $$Props["open"];
-		activeSnapPoint: $$Props["activeSnapPoint"];
-		rest: any;
+		children: Snippet;
+		shouldScaleBackground?: $$Props["shouldScaleBackground"];
+		open?: $$Props["open"];
+		activeSnapPoint?: $$Props["activeSnapPoint"];
+		rest?: any;
 	} = $props();
 
-	// export let shouldScaleBackground: $$Props["shouldScaleBackground"] = true;
-	// export let open: $$Props["open"] = false;
-	// export let activeSnapPoint: $$Props["activeSnapPoint"] = undefined;
 </script>
 
 <DrawerPrimitive.Root {shouldScaleBackground} bind:open bind:activeSnapPoint {...rest}>
