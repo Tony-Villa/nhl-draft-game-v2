e233da497d6dbb9595e18864bb4416468d92ebb3
Tony-Villa <tonyvilla77@gmail.com>
2024-04-25 16:11:45 -0700

    fix: svelte-5ify shadcn drawer

--- src/lib/components/ui/drawer/drawer.svelte
+++ src/lib/components/ui/drawer/drawer.svelte
@@ -2,11 +2,21 @@
 	import { Drawer as DrawerPrimitive } from "vaul-svelte";
 
 	type $$Props = DrawerPrimitive.Props;
-	export let shouldScaleBackground: $$Props["shouldScaleBackground"] = true;
-	export let open: $$Props["open"] = false;
-	export let activeSnapPoint: $$Props["activeSnapPoint"] = undefined;
+
+	let {children, shouldScaleBackground, open = false, activeSnapPoint, ...rest}: 
+	{
+		children: any;
+		shouldScaleBackground: $$Props["shouldScaleBackground"];
+		open:  $$Props["open"];
+		activeSnapPoint: $$Props["activeSnapPoint"];
+		rest: any;
+	} = $props();
+
+	// export let shouldScaleBackground: $$Props["shouldScaleBackground"] = true;
+	// export let open: $$Props["open"] = false;
+	// export let activeSnapPoint: $$Props["activeSnapPoint"] = undefined;
 </script>
 
-<DrawerPrimitive.Root {shouldScaleBackground} bind:open bind:activeSnapPoint {...$$restProps}>
-	<slot />
+<DrawerPrimitive.Root {shouldScaleBackground} bind:open bind:activeSnapPoint {...rest}>
+	{@render children()}
 </DrawerPrimitive.Root>
