4f954b6f4cb4758b78d83ba55619afd2b761d69d
Tony-Villa <tonyvilla77@gmail.com>
2024-06-23 14:04:58 -0700

    fix: get rid of annyong ssr errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	ssr: {
+		noExternal: ['flowbite-svelte', 'bits-ui', 'svelte-french-toast']
 	}
 });
