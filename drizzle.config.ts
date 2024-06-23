ee58f28d5d2af76f66c72a292a8e61d764ffa0b3
Tony-Villa <tonyvilla77@gmail.com>
2024-06-22 18:17:14 -0700

    fix: drizzle update | idk if I messed something up here

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
-import 'dotenv/config';
-import type { Config } from 'drizzle-kit';
+import type { Config } from "drizzle-kit";
 
 export default {
-	schema: './src/lib/server/db/schema',
-	driver: 'turso',
-	dbCredentials: {
-		url: process.env.VITE_TURSO_DB_URL!,
-		authToken: process.env.VITE_TURSO_DB_AUTH_TOKEN
-	},
-	dialect: 'sqlite',
-	out: './drizzle'
+  schema: './src/lib/server/db/schema',
+  out: "./drizzle/migrations",
+  dialect: "sqlite",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.VITE_TURSO_DB_URL!,
+    authToken: process.env.VITE_TURSO_DB_AUTH_TOKEN_STAGING,
+  },
 } satisfies Config;
