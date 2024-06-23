147bb84ca8f4b35d33df19a2edc6a8d50e265111
Tony-Villa <tonyvilla77@gmail.com>
2024-06-23 14:12:46 -0700

    fix: add game phases to page layout

--- src/routes/draft-center/+page.svelte
+++ src/routes/draft-center/+page.svelte
@@ -9,6 +9,12 @@
 	import { getDraftSystem, setDraftSystem } from '$lib/globalState/prospectsState.svelte';
 	import { setCurrentUser } from '$lib/globalState/userState.svelte';
 	import { setDraftState } from '$lib/globalState/draftState.svelte';
+	import HeadToHead from '$lib/components/HeadToHead.svelte';
+	import Countdown from '$lib/components/Countdown.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import { invalidate, invalidateAll } from '$app/navigation';
+	import { differenceInSeconds, format, isAfter, isBefore } from 'date-fns';
+
 
 
 	let { children, data }: {
@@ -19,13 +25,17 @@
 	
 	let playersDrafted = $state(0);
 	let draftBoard = $state(data.draftBoard);
+
+	// console.log('data: ', data);
 	
 	// setCurrentUser(data?.user?.user);
 	setDraftSystem(data.prospects, data.draftBoard, data.emptyDraftBoard);
-	setDraftState()
+	setDraftState(data.game.gamePhase)
+
 
 	const storedDraftBoard = getDraftSystem()
 	const draftState = getDraftState();
+	const draftSystem = getDraftSystem();
 	
 
 	function checkForSavedDraftBoard() {
@@ -42,7 +52,7 @@
 			draftBoard = data.draftBoard;
 		}
 
-		const draftSystem = setDraftSystem(data.prospects, draftBoard, data.emptyDraftBoard);
+		setDraftSystem(data.prospects, draftBoard, data.emptyDraftBoard);
 	}
 
 	let innerWidth = $state(0);
@@ -60,38 +70,74 @@
 		checkForLocalDraftBoard();
 	})
 
+	// $effect(() => {
+	// 	// setDraftState(data.game.gamePhase)
+	// 	if(data.game.gamePhase !== draftState.currentState) {
+	// 		draftState.currentState = data.game.gamePhase
+	// 	}
+	// })
+
 	$effect(() => {
 		if (draftState.currentState === "started") {
 			const socket = new WebSocket("ws://localhost:3000/nhlDraftFeed")
 			socket.onopen = () => {
+				console.log("Connecting to WS");
 				socket.send("start")
 				socket.onmessage = (event) => {
 					let jsonData = JSON.parse(event.data)
-					draftState.currentNhlDraft(jsonData)
+					if(jsonData){
+						draftState.updateNhlDraftPick(jsonData.length)
+
+						for(let i = 0; i < jsonData.length; i++) {
+								draftSystem.nhlDraftBoard[i].prospect = {
+									name: jsonData[i].name
+								} as any
+						}
+					} else {
+						draftState.updateNhlDraftPick(0)
+					}
 	
-					console.log(draftState.nhlDraft);
+					// console.log(draftState.nhlDraft);
 				}
 			}
 		}
 	})
 
+
+	function refresh() {
+		invalidateAll()
+	}
+
+
 </script>
 
 <svelte:window bind:innerWidth />
 <div class="mx-auto max-w-screen-2xl">
-	<h1 class="mb-12 text-center text-6xl font-bold uppercase">draft center</h1>
+	<h1 class="mb-4 text-center text-6xl font-bold uppercase">draft center</h1>
+
+	{#if isAfter(new Date(data.game.startDate), Date.now())}
+		<div class="text-center mb-6">
+			<Countdown endTime={data.game.startDate} />
+			<!-- <Countdown endTime={"2024-06-21T09:05:30Z"} /> -->
+		</div>
+	{/if}
+	
+	{#if draftState.currentState === "started"}
+		<HeadToHead />
+	{/if}
+
 	{#if draftState.currentState !== "started"}
 	<div class=" flex gap-5 px-2">
 		{#if innerWidth < 768}
 			<div class="w-full pb-10">
 				{#if selectedTab === tabs[1]}
-					<DraftBoard draftType="user" />
+					<DraftBoard lockDate={data?.game?.lockDate} draftType="user" />
 				{:else if selectedTab === tabs[0]}
 					<ProspectContainer />
 				{/if}
 			</div>
 		{:else}
-			<DraftBoard draftType="user" />
+			<DraftBoard lockDate={data?.game?.lockDate} draftType="user" />
 			<ProspectContainer />
 		{/if}
 	</div>
@@ -105,13 +151,13 @@
 		{#if innerWidth < 768}
 			<div class="w-full pb-10">
 				{#if selectedTab === tabs[1]}
-					<DraftBoard draftType="user" />
+					<DraftBoard lockDate={data.game.lockDate} draftType="user" />
 				{:else if selectedTab === tabs[0]}
 					<DraftBoard draftType="nhl" />
 				{/if}
 			</div>
 		{:else}
-			<DraftBoard draftType="user" />
+			<DraftBoard lockDate={data.game.lockDate} draftType="user" />
 			<DraftBoard draftType="nhl" />
 		{/if}
 		</div>
