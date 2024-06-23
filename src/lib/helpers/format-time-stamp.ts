05b68333e83db4fee460ce38ba67ba84f3255a2a
Tony-Villa <tonyvilla77@gmail.com>
2024-06-23 00:27:02 -0700

    fet: add helpers

--- src/lib/helpers/format-time-stamp.ts
+++ src/lib/helpers/format-time-stamp.ts
@@ -1,6 +1,9 @@
-export function formatTimeStamp() {
-  const d = new Date()
-  const date = d.toISOString().split('T')[0];
-  const time = d.toTimeString().split(' ')[0];
-  return `${date} ${time}`
+export function formatTimeStampToUTC(timeStamp: string) {
+  // const d = new Date()
+  // const date = d.toISOString().split('T')[0];
+  // const time = d.toTimeString().split(' ')[0];
+  // return `${date} ${time}`
+
+  return timeStamp.replace(' ', 'T')+"Z"
+
 }
