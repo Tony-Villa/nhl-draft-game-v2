444e17c84abb3caea21653d61e90d4fb06588aba
Tony-Villa <tonyvilla77@gmail.com>
2024-06-23 14:45:25 -0700

    fix: remove staging auth token from staging

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,11 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-import { VITE_TURSO_DB_URL, VITE_TURSO_DB_AUTH_TOKEN, VITE_TURSO_DB_AUTH_TOKEN_STAGING } from '$env/static/private';
+import { VITE_TURSO_DB_URL, VITE_TURSO_DB_AUTH_TOKEN } from '$env/static/private';
 import { dev } from '$app/environment';
-
-// const url = dev ? 'http://127.0.0.1:8080' : VITE_TURSO_DB_URL;
-// const url = VITE_TURSO_DB_URL;
-const authToken = dev ? VITE_TURSO_DB_AUTH_TOKEN_STAGING : VITE_TURSO_DB_AUTH_TOKEN
 
 const client = createClient({
 	url: VITE_TURSO_DB_URL,
