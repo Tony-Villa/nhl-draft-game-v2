0d419d4f0e6f14f5b879cdf0fb8580aa2309a5d0
Tony-Villa <tonyvilla77@gmail.com>
2024-06-23 00:28:26 -0700

    breaking: removed players array from game schema

--- src/lib/server/db/schema/games.ts
+++ src/lib/server/db/schema/games.ts
@@ -3,12 +3,12 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const games = sqliteTable(
   'game',
   {
-    id: integer('id').notNull().primaryKey().unique(),
+    id: text('id').notNull().primaryKey().unique(),
     year: text('year').notNull(),
     lockDate: text('lock_date').notNull(),
     startDate: text('start_date').notNull(),
     endDate: text('end_date').notNull(),
-    players: text('players', {mode: 'json'}).$type<string[]>(),
+    gamePhase: text('game_phase').notNull().default('open'),
     defaultGame: integer('default_game', {mode: "boolean"}).notNull().default(false)
   }
 
