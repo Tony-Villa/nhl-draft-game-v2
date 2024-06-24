5eda4ec7a61aad4a1bc37e53acd700e8ec745540
Tony-Villa <tonyvilla77@gmail.com>
2024-06-23 17:52:44 -0700

    fix: add days to countdown

--- src/lib/components/Countdown.svelte
+++ src/lib/components/Countdown.svelte
@@ -10,9 +10,21 @@
 
   let secondsRemaining = $state(differenceInSeconds(end, now))
 
-  let h = $derived(Math.floor(secondsRemaining / 3600));
-  let m = $derived(Math.floor((secondsRemaining - h * 3600) / 60))
-  let s = $derived(secondsRemaining - h * 3600 - m * 60)
+  // let d = $derived(Math.floor(secondsRemaining / (24*60*60)))
+  // let h = $derived(Math.floor(secondsRemaining / 3600));
+  // let m = $derived(Math.floor((secondsRemaining - h * 3600) / 60))
+  // let s = $derived(secondsRemaining - h * 3600 - m * 60)
+
+  let {days, hours, minutes ,seconds} = $derived(convertSecToDHMS(secondsRemaining))
+
+  function convertSecToDHMS(secondsRemaining: number) {
+    const days = Math.floor(secondsRemaining / (3600 * 24))
+    const hours = Math.floor((secondsRemaining % (3600 * 24)) / 3600)
+    const minutes = Math.floor((secondsRemaining % 3600) / 60)
+    const seconds = secondsRemaining % 60
+
+    return {days, hours, minutes, seconds}
+  }
 
   function updateTimer() {
       now = Date.now();
@@ -52,11 +64,30 @@ function zeroPad(n: number | string) {
 
 {#if secondsRemaining > 0}
   <div class="font-bold text-3xl">
-    <h2>
-      <!-- {`${lessThan10(h)}${h}:${lessThan10(m)}${m}:${lessThan10(s)}${s}`} -->
-      {`${zeroPad(h)}:${zeroPad(m)}:${zeroPad(s)}`}
-    </h2> 
+    <div class="flex gap-2 justify-center">
+      {@render CountdownSection(days, 'days')}
+      {@render CountdownClockSeparator()}
+      {@render CountdownSection(hours, 'hours')}
+      {@render CountdownClockSeparator()}
+      {@render CountdownSection(minutes, 'minutes')}
+      {@render CountdownClockSeparator()}
+      {@render CountdownSection(seconds, 'seconds')}
+    </div>
     <h2>Until the NHL Draft</h2>
   </div>
 {/if}
+
+{#snippet CountdownClockSeparator()}
+  <h2 class="font-bold text-3xl">
+    :
+  </h2>
+{/snippet}
     
+{#snippet CountdownSection(t, label)}
+  <div class="flex flex-col">
+    <h2 class="font-bold text-3xl">
+      {zeroPad(t)}
+    </h2>
+    <p class="text-xs">{label}</p>
+  </div>
+{/snippet}
