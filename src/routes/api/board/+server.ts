59ada288629ffc09a4c8ba1281d3432e52e501df
Tony-Villa <tonyvilla77@gmail.com>
2025-01-24 17:35:38 -0800

    fix: handle user and nhl board accepting game id

--- src/routes/api/board/+server.ts
+++ src/routes/api/board/+server.ts
@@ -3,11 +3,14 @@ import { db } from '$lib/server/db/index.js'
 import { nhlDraft } from '$lib/server/db/schema'
 import { eq } from 'drizzle-orm'
 
-export async function GET() {
+export async function GET({url}) {
+
+  const gameId = url.searchParams.get('game')
+
   try {
 		const draftboard =  await getDraftBoardOrder()
 
-    const nhlBoard = await  db.select().from(nhlDraft).where(eq(nhlDraft.gameId, 1)) 
+    const nhlBoard = await  db.select().from(nhlDraft).where(eq(nhlDraft.gameId, +gameId!)) 
 
     if(nhlBoard.length > 0) {
       nhlBoard.forEach(pick => {
--- src/routes/api/userBoard/+server.ts
+++ src/routes/api/userBoard/+server.ts
@@ -1,17 +1,25 @@
+import { CURRENT_GAME } from '$env/static/private';
 import { db } from '$lib/server/db/index.js'
 import { drafts } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
 
 export async function GET(event) {
+	const id = event.url.searchParams.get('board')
+	const game_id = event.url.searchParams.get('game') || CURRENT_GAME
+
   try {
-		const id = event.url.searchParams.get('board')
 
 		if(!id || id !== event.locals.user?.id) return new Response(JSON.stringify({savedDraftBoard: []})) 
 
 		const savedDraftBoard = await db
 				.select()
 				.from(drafts)
-				.where(eq(drafts.userId, id));
+				.where(
+					and(
+						eq(drafts.userId, id), 
+						eq(drafts.gameId, game_id)
+					)
+				);
 		
 
 		return new Response(JSON.stringify(savedDraftBoard), {
