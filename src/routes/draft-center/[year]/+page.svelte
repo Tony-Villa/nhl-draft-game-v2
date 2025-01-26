65d49c40bd6ef16365b2767b9f03c05bee3dcde1
Tony-Villa <tonyvilla77@gmail.com>
2025-01-26 15:05:26 -0800

    feat: wip | ladder page

---
+++ src/routes/draft-center/[year]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Ladder from '$lib/components/Ladder.svelte';
+
+
+  let {data} = $props();
+
+</script>
+  
+<div class="mx-auto h-full max-w-screen-2xl">
+  <Ladder ladder={data?.ladder} />
+</div>
