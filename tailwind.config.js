d9594ade653c1f816a943e33cebd748c0accebd2
Tony-Villa <tonyvilla77@gmail.com>
2025-03-08 12:51:24 -0800

    feat: new font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,14 +21,23 @@ const config = {
 				ring: "hsl(var(--ring) / <alpha-value>)",
 				background: "hsl(var(--background) / <alpha-value>)",
 				foreground: "hsl(var(--foreground) / <alpha-value>)",
-				primary: {
-					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
-				},
+
+				"primary": "#ff2d55",
+				"secondary": "#0055ff",
+				"accent": "#ffdf00",
+				"black": "#000000",
+				"white": "#ffffff",
+				"offWhite": "#f2f2f2",
+				"gray": "#888888",
+
+				// primary: {
+				// 	DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+				// 	foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+				// },
+				// secondary: {
+				// 	DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+				// 	foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+				// },
 				destructive: {
 					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
 					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
@@ -37,10 +46,10 @@ const config = {
 					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
 					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
 				},
-				accent: {
-					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
-				},
+				// accent: {
+				// 	DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+				// 	foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+				// },
 				popover: {
 					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
 					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
@@ -67,7 +76,8 @@ const config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+
+				sans: ["Courier New", ...fontFamily.sans]
 			},
 			boxShadow: {
 				'brut-shadow': '4px 4px 0px 0px #000000',
