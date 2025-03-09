3df229d7e777ff95ac0a0340553d0e1dd78521bc
Tony-Villa <tonyvilla77@gmail.com>
2025-03-09 03:22:00 -0700

    fix: add correct letter spacing to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,4 +5,4 @@
 </script>
     
 
-<h1 class="text-center text-6xl font-bold uppercase [text-shadow:3px_3px_0_#ff2d55]">{title}</h1>
+<h1 class="text-center text-6xl font-bold uppercase [text-shadow:3px_3px_0_#ff2d55] tracking-[-1px]">{title}</h1>
