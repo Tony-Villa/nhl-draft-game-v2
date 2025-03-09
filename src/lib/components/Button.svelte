fad37027f4adfb40059956b897d63105dfabe82d
Tony-Villa <tonyvilla77@gmail.com>
2025-03-08 22:43:35 -0800

    feat: add class to button for overrides

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { twMerge } from 'tailwind-merge';
   import {buttonOptions, type ButtonProps} from './Button.options' 
 
 
-	const { children, disabled = false,  variant = 'primary', size = 'md', skew = 'none', shadow = 'md', id, onclick }: ButtonProps = $props();
+	const { children, disabled = false,  variant = 'primary', size = 'md', skew = 'none', shadow = 'md', class: className, id, onclick }: ButtonProps = $props();
 </script>
 
-<button disabled={disabled} id={id} {onclick} class={buttonOptions({variant, size, shadow, skew})}>
+<button disabled={disabled} id={id} {onclick} class={`${twMerge(buttonOptions({variant, size, shadow, skew}), className)}`}>
     {@render children()}
 </button>
