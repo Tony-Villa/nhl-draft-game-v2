e61dcb469f9dd3f27c89ab14b12edbb9c8225fb9
Tony-Villa <tonyvilla77@gmail.com>
2025-03-15 00:45:38 -0700

    chore: round 3 testing

--- src/routes/draft-center/+page.server.ts
+++ src/routes/draft-center/+page.server.ts
@@ -29,6 +29,7 @@ export const load = async ({ request, setHeaders, locals, fetch }: RequestEvent)
 
 	// testing cicd webhook, will delete later
 	// testing cicd webhook, will delete later
+	// testing cicd webhook, will delete later
 
 	const cached = await redis.get('top_prospects');
 	let topProspects: Prospect[] = [];
