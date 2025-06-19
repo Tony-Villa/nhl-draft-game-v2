452f70a7951903828af29030a8938d48b0d68209
Tony-Villa <tonyvilla77@gmail.com>
2025-06-18 21:15:49 -0700

    chore: use correct json import

--- src/routes/api/seed/+server.ts
+++ src/routes/api/seed/+server.ts
@@ -3,9 +3,9 @@ import { json } from '@sveltejs/kit';
 // import { drafts } from '$lib/server/db/schema/drafts';
 import type { DraftBoard, Prospect } from '$lib/types.js';
 // import { generateId } from 'lucia';
-import { PointsSystem } from '$lib/globalState/scoringSystem.svelte.js';
+import { PointsSystem } from '$lib/global-state/scoring-system.svelte.js';
 import { queryGame } from '$lib/server/db/queries/query-game.js';
-// import { getPointsSystem } from '$lib/globalState/scoringSystem.svelte.js';
+// import { getPointsSystem } from '$lib/global-state/scoring-system.svelte.js';
 
 export async function POST({ request }) {
 	const { data } = await request.json();
@@ -51,41 +51,18 @@ export async function POST({ request }) {
     const tempDraftBoard = draftboard
 
     tempDraftBoard.forEach((draftPick: DraftBoard) => {
-      // console.log('TOP LEVEL DRAFT PICK: ', draftPick);
-
       if(draftPick.prospect !== null) return;
 
       for(let i = 0; i < tempPTiers.length; i++) {
         if(draftPick.prospect !== null) break;
         if(tempPTiers[i].length !== 0) {
-          // console.log('CURRENT PROSPECT TIER: ',i, tempPTiers[i]);
           const selectedProspect = tempPTiers[i].splice(randomIndex(tempPTiers[i]), 1)[0];
-          // console.log('SELECTED PROSPECT: ', selectedProspect);
           draftPick.prospect = selectedProspect;
-
-          // console.log('DRAFT PICK! ', draftPick);
         }
       }
     })
 
-      // console.log(tempDraftBoard );
-
-      // test to see if point system works
-
-      // const pointSystem = new PointsSystem()
-
-      // console.log('before draft: ',pointSystem.officialNhlDraft);
-      
-      // tempDraftBoard.forEach(pick => {
-      //   pointSystem.officialNhlDraftPickMade({
-      //     pick: pick.draftPosition,
-      //     prospect: pick.prospect?.name || ''
-      //   })
-      // })
-      
-      // console.log('after draft: ', pointSystem.officialNhlDraft);
     }
-
 
   // ******************* CREATE USERS AND DRAFTS FOR THEM ***************
 	// try {
@@ -105,9 +82,7 @@ export async function POST({ request }) {
 	// 		});
 	// 	});
 	// } catch (error) {
-	// 	console.log(error);
 	// }
-
 
 	return json({ message: 'success' });
 }
