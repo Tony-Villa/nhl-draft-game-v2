871421f7b9b55f5311ab7422a4a3d7f83a34997d
Tony-Villa <tonyvilla77@gmail.com>
2025-06-19 20:06:26 -0700

    fix: board goes to 1 card at a larger width

--- src/lib/components/DraftBoard.svelte
+++ src/lib/components/DraftBoard.svelte
@@ -115,14 +115,16 @@
 
 	<div class={`draft-card-container mb-16 flex flex-wrap justify-center gap-2 `}>
 		{#each draftBoard || [] as position}
-			<Card size="sm" class={`draft-card flex items-center ${draftBoardContainerWidth > 300 ? 'basis-[48%]' : 'basis-[100%]'} max-[430px]:basis-[100%]`}>
-				<div class="flex items-center px-2 py-2">
-					<h2 class="text-black font-extrabold text-[28px]">{position.draftPosition}</h2>
-					<div class="relative">
-						<img class="h-[50px] w-[50px] z-10" src={position.teamLogo} alt="" />
-						{#if position?.from}
-							<img class="h-6 w-6 absolute right-0 top-[64%]" src={position.from} alt=""/>
-						{/if}
+			<Card size="sm" class={`draft-card flex items-center ${draftBoardContainerWidth > 500 ? 'basis-[48%]' : 'basis-[100%]'} max-[430px]:basis-[100%]`}>
+				<div class="flex items-center px-2 py-2 justify-between w-full">
+					<div  class="flex items-center px-2 py-2">
+						<h2 class="text-black font-extrabold text-[28px]">{position.draftPosition}</h2>
+						<div class="relative">
+							<img class="h-[50px] w-[50px] z-10" src={position.teamLogo} alt="" />
+							{#if position?.from}
+								<img class="h-6 w-6 absolute right-0 top-[64%]" src={position.from} alt=""/>
+							{/if}
+						</div>
 					</div>
 					{#if position.prospect}
 					<div in:fade class='flex flex-1 justify-between items-center'>
