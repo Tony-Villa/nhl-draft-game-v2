b0509d74501c54d17feb8ccba1ecea72618d3f92
Tony-Villa <tonyvilla77@gmail.com>
2025-06-25 16:20:17 -0700

    fix: mobile draftboard

--- src/lib/components/DraftBoard.svelte
+++ src/lib/components/DraftBoard.svelte
@@ -54,7 +54,7 @@
 </script>
 
 <div bind:clientWidth={draftBoardContainerWidth} class={`
-	flex-1 max-h-fit p-6 bg-white border-black border-[5px] relative mb-7 md:shadow-section-shadow md:-rotate-[0.3deg]
+	flex-1 max-h-fit p-6 bg-white border-black border-[5px] relative mb-7 md:shadow-section-shadow z-0 md:-rotate-[0.3deg]
 `}>
 	<h2 class={`
 	text-3xl font-extrabold uppercase tracking-[-1px] relative inline-block mb-7
